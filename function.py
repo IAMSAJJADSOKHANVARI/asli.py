6dec181b6991d5bdb6705956f9778e18f6c16616
sajjadsokhanvari <sajjadnetworker2015@gmail.com>
2023-09-06 00:35:01 +0330

    Implement the Edit button

--- function.py
+++ function.py
@@ -13,7 +13,7 @@ def write_todos(todos_arg, filepath='files/text_file.txt'):
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
 
-print(__name__)
+# print(__name__)
 if __name__ == "__main__":
     print("Hello from function! ")
     print(get_todos())
--- gui.py
+++ gui.py
@@ -1,24 +1,40 @@
 import function
-import PySimpleGUI as sq
+import PySimpleGUI as sg
 
-label = sq.Text("Type in a to_do")
-input_box = sq.InputText(tooltip="Enter todo", key="todo")
-add_button = sq.Button("Add")
+label = sg.Text("Type in a to_do  sajjad :)")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
+add_button = sg.Button("Add")
+list_box = sg.Listbox(values=function.get_todos(), key='todos',
+                      enable_events=True, size=[45, 10])
 
-window = sq.Window("My To_Do App",
-                   layout=[[label], [input_box], [add_button]],
+edit_button = sg.Button("Edit")
+
+window = sg.Window("My To_Do App",
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
                    font=('Helvetica', 20))
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
+    print(1, event)
+    print(2, values)
+    print(3, values['todos'])
     match event:
         case 'Add':
             todos = function.get_todos()
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             function.write_todos(todos)
-        case sq.WIN_CLOSED:
+            window['todos'].update(values=todos)
+        case "Edit":
+            todo_to_edit = values["todos"][0]
+            new_todo = values['todo']
+            todos = function.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            function.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "todos":
+            window['todo'].update(value=values['todos'][0])
+        case sg.WIN_CLOSED:
             break
 
 window.close()
