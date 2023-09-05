7702d0a97e323af56ae32edac271400c3490b895
sajjadsokhanvari <sajjadnetworker2015@gmail.com>
2023-09-05 19:24:39 +0330

    Implement the ad button

--- gui.py
+++ gui.py
@@ -2,10 +2,23 @@ import function
 import PySimpleGUI as sq
 
 label = sq.Text("Type in a to_do")
-input_box = sq.InputText(tooltip="Enter todo")
+input_box = sq.InputText(tooltip="Enter todo", key="todo")
 add_button = sq.Button("Add")
 
-window = sq.Window("My To_Do App", layout=[[label], [input_box],[ add_button]])
-window.read()
-print("Hello")
+window = sq.Window("My To_Do App",
+                   layout=[[label], [input_box], [add_button]],
+                   font=('Helvetica', 20))
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case 'Add':
+            todos = function.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            function.write_todos(todos)
+        case sq.WIN_CLOSED:
+            break
+
 window.close()
