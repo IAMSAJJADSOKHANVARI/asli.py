4a15678a1b2731e0aa8f6b45d95825394a59fbfd
sajjadsokhanvari <sajjadnetworker2015@gmail.com>
2023-09-07 08:51:08 +0330

    create new feture

--- web.py
+++ web.py
@@ -5,17 +5,23 @@ todos = function.get_todos()
 
 
 def add_todo():
-    todo = st.session_state["new_todo"]+'\n'
+    todo = st.session_state["new_todo"] + '\n'
     todos.append(todo)
     function.write_todos(todos)
+
 
 st.title("Sajjad Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity. ")
 st.title("My Todo App")
-for todo in todos:
-    st.checkbox(todo)
 
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        function.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun
 st.text_input(label="", placeholder="Add a new todo...",
               on_change=add_todo, key='new_todo')
 
