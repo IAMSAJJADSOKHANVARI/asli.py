52fd603795aa26bce12074e2cf72b29b344737e1
sajjadsokhanvari <sajjadnetworker2015@gmail.com>
2023-09-07 05:42:30 +0330

    change to the icon exit,edit,complet and add for program

--- gui.py
+++ gui.py
@@ -7,13 +7,18 @@ sg.theme("Green")
 clock = sg.Text('', key='clock')
 label = sg.Text("Type in a to_do  sajjad :)")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add",size=10)
+add_button = sg.Button(size=10,image_source='2.png',mouseover_colors="LightBlue",
+                       tooltip="Add todo",key="Add")
 list_box = sg.Listbox(values=function.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
 
-edit_button = sg.Button("Edit")
-complete_button = sg.Button("Complete")
-exit_button = sg.Button("Exit")
+edit_button = sg.Button(size=10,image_source="edit.png",mouseover_colors="LightBlue"
+                        ,tooltip="Edit",key="Edit")
+complete_button = sg.Button(size=10,image_source="004 complete.png",
+                            mouseover_colors="LightBlue",tooltip="complete",
+                            key="Complete",image_size=(30,30))
+exit_button = sg.Button(size=10,image_source="exit.png",mouseover_colors="LightBlue"
+                        ,tooltip="Exit",key="Exit",)
 
 window = sg.Window("My To_Do App",
                    layout=[[clock],
